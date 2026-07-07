3396e7671a0208eb67e213e034e6c2a4eb256dbf
Luis1f2 <221271@ids.upchiapas.edu.mx>
2026-07-06 23:26:48 -0600

    conexion api

--- src/core/shared/config/api.ts
+++ src/core/shared/config/api.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = 'https://explora-chiapas.onrender.com/v1/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error('Falta configurar VITE_API_BASE_URL');
+}
+
+export const BASE_URL = API_BASE_URL;
