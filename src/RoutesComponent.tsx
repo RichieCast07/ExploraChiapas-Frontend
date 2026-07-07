45105b94ba56a45baaec41cc38b884dce58b11c1
wilabarca <wilberabarcasanchez078@gmail.com>
2026-07-06 18:17:37 -0600

    Integration vista eventos

--- src/RoutesComponent.tsx
+++ src/RoutesComponent.tsx
@@ -4,6 +4,7 @@ import LoginPage from './features/Auth/presentation/pages/Login/Login';
 import RegisterPage from './features/Auth/presentation/pages/Register/Register';
 import { NegocioHomePage } from './features/NegocioTuristico/Home/presentation/pages/NegocioHomePage';
 import { PromocionesPage } from './features/NegocioTuristico/Promociones/presentation/pages/PromocionesPage';
+import { RegistroNegocio } from './features/NegocioTuristico/RegistroNegocio/presentation/pages/RegistroNegocio';
 import { Eventos } from './features/SistemaAdministrador/Eventos/presentation/pages/Eventos';
 import { HomeEventos } from './features/SistemaAdministrador/Eventos/presentation/pages/HomeEventos';
 
@@ -16,6 +17,7 @@ const RoutesComponent = () => {
 
       <Route path="/negocio/inicio" element={<NegocioHomePage />} />
       <Route path="/negocio/promociones" element={<PromocionesPage />} />
+      <Route path="/negocio/registrar" element={<RegistroNegocio />} />
 
       <Route path="/administrador/eventos" element={<HomeEventos />} />
       <Route path="/administrador/eventos/nuevo" element={<Eventos />} />
